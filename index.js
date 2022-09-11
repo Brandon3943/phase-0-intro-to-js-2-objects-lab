1aa259f8ad3e9ca1a6f29164f6de1fd03a14061f
root <root@Ty-Laptop.localdomain>
2022-09-11 14:40:14 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, 
+            "name": "Sam", 
+            "streetAddress": "11 Broadway"}
+}
+
+
+
+//update
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee["streetAddress"] = "12 Broadway";
+
+    return employee;
+}
+
+//delete
+function deleteFromEmployeeByKey(employee, key) {
+    employee = {
+        name: "Sam"
+    }
+    delete employee.name;  
+
+    return employee;
+}
+
+
+//
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name;
+    return employee;
+}
+
+
+
+
+console.log(employee)
